247874e83500e67829b7829c70f2e3744010dd5d
dtjproNotStolen <dtjpro.social@gmail.com>
2024-08-29 22:53:12 +0200

    additonal line

--- main.cpp
+++ main.cpp
@@ -7,10 +7,12 @@ int main() {
     str name;       //variable asigment
 
     std::cout << "Whats your name:";    //input
-    std::cin >> name;
+    std::cin >> name;   /*if i need input wight spaces
+    std::getline(std::cin, name);
+    */
 
     std::cout << "Hello " << name << '\n';  //final output
 
 
-    return 0;
+    return 0; //sorry for gramar and words im writing this on 11pm
 }
