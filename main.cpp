aafb519b5e3f8ea7877fb733b15b3ba253cb305d
dtjproNotStolen <dtjpro.social@gmail.com>
2024-08-29 21:30:38 +0200

    testing

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+//typedef std::string str; //not wana use fulll name, I'm lazy
+using str = std::string;    //lol just found out how to do it better
+
+int main() {
+    str name = "bob";
+
+    std::cout << name << std::endl;
+
+    return 0;
+}
