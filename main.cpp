5c6edb314aa9ce160277af06ae18be4949f494e1
dtjproNotStolen <dtjpro.social@gmail.com>
2024-08-30 08:58:48 +0200

    done and tested

--- main.cpp
+++ main.cpp
@@ -1,9 +1,51 @@
 #include <iostream>
 
-//typedef std::string str; //not wana use fulll name, I'm lazy
-using str = std::string;    //lol just found out how to do it better
+using str = std::string;
 
 int main() {
+
+    char input;
+    double number1;
+    double number2;
+    double resoult;
+
+    std::cout << "*******************************************\n";
+    std::cout << "************condsole calculator************\n";
+    std::cout << "*******************************************\n";
+
+
+    std::cout << "Enter an type of operation (+, -, /, *) :";
+    std::cin >> input;
+
+    std::cout << "\nEnter the first number:";
+    std::cin >> number1;
+
+    std::cout << "\nEnter the second number:";
+    std::cin >> number2;
+
+    
+
+    switch (input){
+    case ('+'):
+        std::cout << "\nThe resoult is:";       //sorry im too lazy to make it better and if it works it works
+        std::cout << number1 + number2 <<'\n';
+        break;
+    case ('-'):
+        std::cout << "\nThe resoult is:";
+        std::cout << number1 - number2 <<'\n';
+        break;
+    case ('/'):
+        std::cout << "\nThe resoult is:";
+        std::cout << number1 / number2 <<'\n';
+        break;
+    case ('*'):
+        std::cout << "\nThe resoult is:";
+        std::cout << number1 * number2<<'\n';
+    default:
+        std::cout << "\nPlease enter an operator\n";
+        break;
+    }
+
 
     return 0;
 }
