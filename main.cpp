ed87c8f1c516d0dda2a40fa6e801e6cd3421644a
dtjproNotStolen <dtjpro.social@gmail.com>
2024-08-31 22:42:48 +0200

    done

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 #include <string>
 
-//typedef std::string str; //not wana use fulll name, I'm lazy
-using str = std::string;    //lol just found out how to do it better
+using str = std::string;
+double calcTotal(double prices[], int size);
 
 int main() {
 
+    double prices[] = {4.99, 1.49 ,3.49, 6.49};
+    int size = sizeof(prices)/sizeof prices[0];
+    double total = calcTotal(prices, );
+
+    std::cout << total << '\n';
+
     return 0;
+}
+
+double calcTotal(double prices[], int size) { //lol
+    double total = 0;
+    for (int i = 0; i < size; i++) {
+        total += prices[i];
+    }
+    return total;
 }
