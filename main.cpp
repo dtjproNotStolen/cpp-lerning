38ae04b9883bae95de67fe6b0b3efb41916465d5
dtjproNotStolen <dtjpro.social@gmail.com>
2024-08-31 13:26:06 +0200

    need to fix winrate

--- main.cpp
+++ main.cpp
@@ -7,19 +7,25 @@ using str = std::string;
 int main() {
     srand(time(0));
 
-    double zeros = 0;
-    double ones = 0;
-    double twos = 0;
+
     int x;
     str input;
     bool doQuit = false;
+    int wins = 0;
+    int score = 0;
+    int loses = 0;
+    int draws = 0;
+    int roundsPlayed = 0;
+    double winRate = 0;
 
-            std::cout << "****************rock paper scisors game*****************\n";
+    std::cout << "****************rock paper scisors game*****************\n";
 
     
     while (!doQuit) {
         x = rand() %3;
         // add game       
+
+
 
         std::getline(std::cin, input);
 
@@ -28,19 +34,86 @@ int main() {
             doQuit = true;
             std::cout <<"exiting\n";
         } else if (input == "help") {
-            std::cout << "rock - plays rock \n";
+
+            
+
+
+            std::cout << "rock s- plays rock \n";
             std::cout << "paper - exits the program \n";
             std::cout << "scissors - exits the program \n";
             std::cout << "info - shows the score \n";
             std::cout << "help - shows the list of comands \n";
             std::cout << "exit - exits the program \n";
-        } else {
-            std::cout << "type \"help\" for the list of commands\n";
+        } else if (input == "info") {
+
+            if (wins + loses != 0){
+                winRate = wins/(wins+loses)*100;
+            }
+            
+
+            std::cout << "wind:" << wins << '\n';
+            std::cout << "loses:" << loses << '\n';
+            std::cout << "draws:" << draws << '\n';
+            std::cout << "rounds played:" << roundsPlayed <<'\n';
+            std::cout << "win rate:" << 0/5 << "%\n";
+        } else if (input == "rock") {
+            if (x == 0){
+                draws++;
+                std::cout << "draw\n";
+            }else if (x == 1)
+            {
+                score--;
+                loses++;
+                std::cout << "lost\n";
+            } else if (x == 2)
+            {
+                score++;
+                wins++;
+                std::cout << "won\n";
+            } else {
+                std::cout << "something went wrong";
+            } 
+        } else if (input == "paper") {
+            if (x == 0){
+                wins++;
+                score++;
+                std::cout << "won\n";
+            }else if (x == 1)
+            {
+                draws++;
+                std::cout << "draw\n";
+            } else if (x == 2)
+            {
+                score--;
+                loses++;
+                std::cout << "lost\n";
+            } else {
+                std::cout << "something went wrong";
+            }
+        } else if (input == "scissors") {
+            if (x == 0){
+                loses++;
+                score--;
+                std::cout << "lose\n";
+            }else if (x == 1)
+            {
+                score++;
+                wins++;
+                std::cout << "won\n";
+            } else if (x == 2)
+            {
+                draws++;
+                std::cout << "tie\n";
+            } else {
+                std::cout << "something went wrong";
+            }
         }
-        
+        if (input == "rock" || input == "paper" || input == "scissors") {
+        roundsPlayed++;
+        }
         
     }
     
-
     return 0;
+
 }
