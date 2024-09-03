b4fd270374bcc9d710def6f59ed03d9461668e19
dtjproNotStolen <dtjpro.social@gmail.com>
2024-09-03 12:05:23 +0200

    done

--- main.cpp
+++ main.cpp
@@ -1,24 +1,32 @@
 #include <iostream>
 #include <string>
 
-int arrayIntSerch(int array, int serchedNum, int lenght);
+int arrayIntSerch(int array[], int serchedNum, int lenght);  
 
-using str = std::string; 
 
 int main() {
 
-    int nums[] = {1,2,3,4,5,6,7,8,9,10};
+    int nums[] = {1,2,3,4,5,6,7,8,9,10};            //decleration of variables
+    int lenght = sizeof(nums) / sizeof(nums[0]);
+    int idkNum;
+
+    std::cin >> idkNum;
+
+    int idkNumPlace;
+
+    idkNumPlace = arrayIntSerch(nums, idkNum, lenght);
+    std::cout << idkNumPlace << '\n';
 
     return 0;
 }
 
-int arrayIntSerch(int array, int serchedNum, int lenght) {
-    for (int i = 0; i < lenght; i++)
-    {
+int arrayIntSerch(int array[], int serchedNum, int lenght) {            // btw you can switch type of serch by switching first and second data type to another type
+    for (int i = 0; i < lenght; i++){
         if (array[i] == serchedNum) {
             return i;
         }
         
     }
+    return -1;
     
 }
